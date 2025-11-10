f60bb7cd7956382e5a11860a83dca398af98e413
Mauricio Barragan <mauricioabh@gmail.com>
2025-11-10 18:52:20 +0000

    fix(settings): include rest_framework.authtoken for dj-rest-auth

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 # Third-party apps for the OctoFit project
 INSTALLED_APPS += [
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'dj_rest_auth',
     'django.contrib.sites',
